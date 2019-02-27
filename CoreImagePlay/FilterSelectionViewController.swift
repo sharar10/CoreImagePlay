2a170a11a81657a6128eaab4c406032994e51f37
Sharar Arzuk Rahman <sharar.rahman@gmail.com>
2019-02-27 12:32:13 -0500

    Set up table view to select from filters

---
+++ CoreImagePlay/FilterSelectionViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  CoreImagePlay
+//
+//  Created by Sharar Arzuk Rahman on 2/27/19.
+//  Copyright © 2019 Sharar Arzuk Rahman. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class FilterSelectionViewController: UIViewController {
+    // MARK: stored properties
+    let reuseIdentifier = "FilterNameCell"
+
+    // MARK: Interface builder outlets
+    @IBOutlet weak var filtersTableView: UITableView!
+
+    // MARK: view lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //playVideo()
+    }
+
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "720PVideo", ofType: "mp4") else {
+            fatalError()
+        }
+        let url = URL(fileURLWithPath: path)
+        let player = AVPlayer(url: url)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        self.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
+
+// MARK: UITableViewDataSource methods
+extension FilterSelectionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) else {
+            return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
+        }
+        cell.textLabel?.text = "TEST TO MAKE SURE IT WORKS"
+        return cell
+    }
+}
+
+// MARK: UITableViewDelegate methods
+extension FilterSelectionViewController: UITableViewDelegate {
+
+}
+
