288f382a015aa95e05e7e70e951b44f425054d2a
Sharar Arzuk Rahman <sharar.rahman@gmail.com>
2019-02-27 12:18:26 -0500

    Added filters that require creation

---
+++ CoreImagePlay/CoreImageModels/GrayscaleFilter.swift
@@ -0,0 +1,14 @@
+//
+//  GrayscaleFilter.swift
+//  CoreImagePlay
+//
+//  Created by Sharar Arzuk Rahman on 2/27/19.
+//  Copyright © 2019 Sharar Arzuk Rahman. All rights reserved.
+//
+
+import Foundation
+import CoreImage
+
+struct GrayscaleFilter {
+
+}
